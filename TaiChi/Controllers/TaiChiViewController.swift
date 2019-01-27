8a0d3520d53d1470e877868b96c3a8f727fc74e6
yaoshi <yaos5@student.unimelb.edu.au>
2019-01-27 10:39:57 +0800

    Implemented loop play feature and a toggle switch

--- TaiChi/Controllers/TaiChiViewController.swift
+++ TaiChi/Controllers/TaiChiViewController.swift
@@ -11,6 +11,19 @@ import AVKit
 
 class TaiChiViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    
+    
+    var isLoopPlayEnabled = false
+    @IBAction func loopSwitch(_ sender: UISwitch) {
+        if (sender.isOn) {
+            isLoopPlayEnabled = true
+        }
+        else {
+            isLoopPlayEnabled = false
+        }
+    }
+    
+    
     let testArray : [String] = ["square.jpg", "square.jpg", "square.jpg"]
     let titleArray : [String] = ["rehab video 1", "rehab video 2", "rehab video 3"]
     
@@ -44,23 +57,56 @@ class TaiChiViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     func playVideo(videoName: String) {
         if let path = Bundle.main.path(forResource: videoName, ofType: "MP4") {
-            let video = AVPlayer(url: URL(fileURLWithPath: path))
+            let url = URL(fileURLWithPath: path)
+            let video = AVPlayer(url: url)
             let videoPlayer = AVPlayerViewController()
             videoPlayer.player = video
             
-            //prot code
-            let interval = CMTime(seconds: 1, preferredTimescale: 1)
-            video.addPeriodicTimeObserver(forInterval: interval, queue: nil) { (time) in
-                let seconds = CMTimeGetSeconds(time)
-                print(Int(seconds))
-            }
+            // Trying to implement looping
+            let fullLength = getVideoLength(url: url)
+            print("fullLength", fullLength)
+            
+            /* Calling this function to track the time played and enable loop play */
+            manageTime(video: video, fullLength: fullLength)
             
             present(videoPlayer, animated: true) {
                 video.play()
             }
-            
         }
     }
     
+    
+    func getVideoLength(url: URL) -> Double {
+        let asset = AVAsset(url: url)
+        let duration = asset.duration
+        // rounding to one decimal place
+        let fullLength: Double = Double(CMTimeGetSeconds(duration) * 10).rounded() / 10
+        
+        return fullLength
+    }
+    
+    
+    func manageTime(video: AVPlayer, fullLength: Double){
+        let interval = CMTime(seconds: 1, preferredTimescale: 1)
+        var seconds : Double = 0.0
+        video.addPeriodicTimeObserver(forInterval: interval, queue: nil) { (time) in
+            seconds = Double(CMTimeGetSeconds(time))
+            print(seconds)
+            
+            /* If it reaches the end of the video, then seek to the beginning */
+            if self.isLoopPlayEnabled {
+                if (fullLength - seconds) < 0.1 {
+                    print("It is now at the end of the video")
+                    let beginning = CMTime(seconds: 0, preferredTimescale: 1)
+                    video.seek(to: beginning)
+                    seconds = 0.00
+                    
+                    /* If I don't pause, sometimes loop play doesn't work */
+                    video.pause()
+                    video.play()
+                }
+            }
+        }
+    }
 
 }
