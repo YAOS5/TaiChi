976f793a45f32e8be3b3e676ebe3287e17dd4abb
yaoshi <yaos5@student.unimelb.edu.au>
2019-02-05 14:46:05 +0800

    Minor bug fix in time calculation, and starting to work on creating JSON objects for exporting

---
+++ TaiChi/Controllers/TaiChiViewControllerFiles/TaiChiJSONExtension.swift
@@ -0,0 +1,9 @@
+//
+//  TaiChiJSONExtension.swift
+//  TaiChi
+//
+//  Created by Peteski Shi on 5/2/19.
+//  Copyright © 2019 Petech. All rights reserved.
+//
+
+import Foundation
---
+++ TaiChi/Controllers/WarmUpViewControllerFiles/WarmUpJSONExtension.swift
@@ -0,0 +1,9 @@
+//
+//  WarmUpJSONExtension.swift
+//  TaiChi
+//
+//  Created by Peteski Shi on 5/2/19.
+//  Copyright © 2019 Petech. All rights reserved.
+//
+
+import Foundation
